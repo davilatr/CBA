68054a1aeef8d89d4077aa5c9f610a839aaab420
Rafael d'Ávila <davilatr@gmail.com>
2020-06-01 22:58:46 -0400

    [Paginação] Possibilidade do cliente selecionar quantidade de registro a ser exibido. Adicionado o recurso bootstrap-select https://developer.snapappointments.com/bootstrap-select/
    [Refatoração] Refatoração de Controllers e Views para aproveitamento de código redundantes

---
+++ CBA.Web/Controllers/Cadastro/DestinacaoBemController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using CBA.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CBA.Web.Controllers
+{
+    public class DestinacaoBemController : Controller
+    {
+        private const int _qtdeMaxLinhasPorPagina = 10;
+
+        // GET: Cadastro
+
+        #region Tipos de Destinação de Bens
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            ViewBag.ListaTamPag = new SelectList(new int[] { _qtdeMaxLinhasPorPagina, 20, 30 }, _qtdeMaxLinhasPorPagina);
+            ViewBag.QtdeMaxLinhasPorPagina = _qtdeMaxLinhasPorPagina;
+            ViewBag.PaginaAtual = 1;
+            var lista = DestinacaoBemModel.RecuperarDestinacaoBem(ViewBag.PaginaAtual, _qtdeMaxLinhasPorPagina);
+            var qtdeReg = DestinacaoBemModel.RecuperarDestinacaoBemQtde();
+
+
+            ViewBag.QtdeDePaginas = (qtdeReg / ViewBag.QtdeMaxLinhasPorPagina);
+            if (qtdeReg % ViewBag.QtdeMaxLinhasPorPagina > 0)
+                ViewBag.QtdeDePaginas++;
+
+
+            return View(lista);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult DestinacaoBemPaginacao(int pagina, int tamPag)
+        {
+            var lista = DestinacaoBemModel.RecuperarDestinacaoBem(pagina, tamPag);
+            return Json(lista);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult ListaDestinacaoBem(int id)
+        {
+            return Json(DestinacaoBemModel.RecuperarDestinacaoBem(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult ExcluirDestinacaoBem(int id)
+        {
+            return Json(DestinacaoBemModel.ExcluirDestinacaoBem(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult SalvarDestinacaoBem(DestinacaoBemModel obj)
+        {
+            var resultado = "ok";
+            var mensagens = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "aviso";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    var id = obj.SalvarDestinacaoBem();
+                    if (id > 0)
+                        idSalvo = id.ToString();
+
+                    else
+                        resultado = "erro";
+
+                }
+                catch (Exception ex)
+                {
+                    resultado = "erro";
+                    Console.WriteLine(ex);
+                    throw;
+                }
+            }
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+
+        #endregion
+
+        
+    }
+}
--- CBA.Web/Controllers/Cadastro/UsuarioController.cs
+++ CBA.Web/Controllers/Cadastro/UsuarioController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace CBA.Web.Controllers
 {
-    public class CadastroController : Controller
+    public class UsuarioController : Controller
     {
         private const int _qtdeMaxLinhasPorPagina = 10;
 
@@ -78,96 +78,6 @@ namespace CBA.Web.Controllers
 
         #endregion
 
-        [Authorize]
-        public ActionResult UnidadeOrganizacional()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult BemApreendido()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult VistoriaBem()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult Pessoa()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult AutoInfracao()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult TermoApreensao()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult TermoDeposito()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult ProcessoAdministrativo()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult TipoBem()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult UnidadeMedida()
-        {
-            return View();
-        }
-
-        //--------------------------------------------------------------------
-
-        [Authorize]
-        public ActionResult UnidadeFederativa()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult Cidade()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult EstadoConservacao()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult RotaVistoria()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult PerfilUsuario()
-        {
-            return View();
-        }
+        
     }
 }
